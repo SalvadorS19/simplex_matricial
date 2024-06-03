d1aa8c8256dc01b74442e114a71de71212f1240f
ssanabria <salvadorsanabria14@gmail.com>
2024-06-02 22:53:15 -0500

    iteraciones full

--- script.js
+++ script.js
@@ -11,14 +11,20 @@ ventana_restricciones_objetivo.style.display = 'none';
 
 // Acciones
 const boton_continuar = document.querySelector("#continuar-btn");
+const mensaje_fin = document.querySelector(".mensaje-fin");
+mensaje_fin.style.display = "none";
 
 // Ventana resultados
 const ventana_resultados = document.querySelector(".ventana-resultados");
 const funcion_obj_final_titulo = document.querySelector("#funcion_obj_final");
 ventana_resultados.style.display = 'none'; 
 
+// Ventana iteraciones
+const ventana_iteraciones = document.querySelector(".ventana-iteraciones");
+ventana_iteraciones.style.display = 'none';
 
 let ventana_actual = "params";
+let iteracion_actual = 1;
 
 let tipo_funcion;
 let cantidad_variables = 0;
@@ -31,8 +37,7 @@ let variables = [];
 let coeficientesFuncion;
 let constantes = [];
 let matrizCoeficientes = [];
-
-setVentanaIteracion();
+let Xb = [];
 
 boton_continuar.addEventListener("click", (event) => {
     console.log(event);
@@ -66,24 +71,305 @@ boton_continuar.addEventListener("click", (event) => {
             ventana_actual = "resultados";
             break;
         case "resultados":
+            ocultarElemento(ventana_resultados);
+            mostrarElemento(ventana_iteraciones);
+            
+            setVentanaIteracion();
             ventana_actual = "iteraciones";
-        default:
+            break;
+        case "iteraciones":
+            limpiarMatrices();
+            iteracion_actual++;
+            setVentanaIteracion();
             break;
     }
 });
 
+function limpiarMatrices() {
+    const ElMatrizB = document.querySelector(".matriz-b .matriz");
+    while (ElMatrizB.firstChild) {
+        ElMatrizB.removeChild(ElMatrizB.lastChild);
+    }
+
+    const bInversaEl = document.querySelector(".matriz-b-1 .matriz");
+    while (bInversaEl.firstChild) {
+        bInversaEl.removeChild(bInversaEl.lastChild);
+    }
+
+    const bInversaAEl = document.querySelector(".matriz-b-1a .matriz");
+    while (bInversaAEl.firstChild) {
+        bInversaAEl.removeChild(bInversaAEl.lastChild);
+    }
+}
+
 function setVentanaIteracion() {
+    const tituloIteracion = document.querySelector("#titulo-iteracion");
+    tituloIteracion.textContent = `Iteración ${iteracion_actual}`;
     const vectorXb = document.querySelector(".vector-xb");
-    const matriz = [
-        [1, 0, 2, 0, 0],
-        [0, 1, 3, 0, 0],
-        [0, 0, 2, 1, 0],
-        [2, 3, 1, 1, 1]
-    ];
-    const columnas = getIdentityMatrixColumns(matriz)
-    console.log(columnas);
-    // const columnasIdentidad = findIdentityMatrixColumns(matriz);
-    //vectorXb.textContent = `[ ${columnasIdentidad.join(", ")} ]`;
+    let posiciones = [], columnas = [];
+    if (!Xb.length) {
+        const resp = getMatrizIdentidad(matrizCoeficientes);
+        posiciones = resp.posiciones;
+        columnas = resp.columnas;
+        Xb = posiciones.map(pos => variables[pos]);
+    } else {
+        Xb.forEach(element => {
+            posiciones.push(variables.indexOf(element));
+        });
+        posiciones.forEach(pos => {
+            let columna = [];
+            for (let i = 0; i < matrizCoeficientes.length; i++) {
+                columna.push(matrizCoeficientes[i][pos]);
+            }
+            columnas.push(columna);
+        });
+    }
+    console.log(posiciones, columnas);
+    vectorXb.textContent = `[ ${Xb.join(", ")} ]`;
+
+    const ElMatrizB = document.querySelector(".matriz-b .matriz");
+    let matrizB = [];
+    const rows = columnas.length;
+    const cols = columnas[0].length;
+    for (let i = 0; i < cols; i++) {
+        let fila = [];
+        for (let j = 0; j < rows; j++) {
+            fila.push(columnas[j][i]);
+        }
+        matrizB.push(fila);
+    }
+    for (let i = 0; i < matrizB.length; i++) {
+        const fila = document.createElement("span");
+        const elementos = matrizB[i].join(", ");
+        fila.textContent = `[ ${elementos} ]`;
+        ElMatrizB.appendChild(fila);
+    }
+
+    const CtbEl = document.querySelector(".vector-ct");
+    const coeficientes = Object.values(coeficientesFuncion);
+    const ctb = posiciones.map(pos => coeficientes[pos]);
+    CtbEl.textContent = `[ ${ctb.join(", ")} ]`;
+
+    const bInversaEl = document.querySelector(".matriz-b-1 .matriz");
+    const bInversa = math.inv(matrizB); 
+    for (let i = 0; i < bInversa.length; i++) {
+        const fila = document.createElement("span");
+        fila.textContent += "[ ";
+        let valores = [];
+        for (let j = 0; j < bInversa[0].length; j++) {
+            const valor = bInversa[i][j] % 1 != 0 
+                ? Number(bInversa[i][j]).toFixed(2) 
+                : bInversa[i][j];
+            valores.push(valor);
+        }
+        fila.textContent += valores.join(", ") + " ]";
+        bInversaEl.appendChild(fila);
+    }
+
+    const bInversaAEl = document.querySelector(".matriz-b-1a .matriz");
+    const bInversaA = multiplicarMatrices(bInversa, matrizCoeficientes);
+    for (let i = 0; i < bInversaA.length; i++) {
+        const fila = document.createElement("span");
+        fila.textContent += "[ ";
+        let valores = [];
+        for (let j = 0; j < bInversaA[0].length; j++) {
+            const valor = bInversaA[i][j] % 1 != 0 
+                ? Number(bInversaA[i][j]).toFixed(2) 
+                : bInversaA[i][j];
+            valores.push(valor);
+        }
+        fila.textContent += valores.join(", ") + " ]";
+        bInversaAEl.appendChild(fila);
+    }
+
+    const ctbaEl = document.querySelector(".vector-ctb");
+    const ctba = vectorPorMatriz(ctb, bInversaA);
+    let valores = [];
+    for (let i = 0; i < ctba.length; i++) {
+        const valor = ctba[i] % 1 != 0 
+            ? Number(ctba[i]).toFixed(2) 
+            : ctba[i];
+        valores.push(valor);
+    }
+    ctbaEl.textContent = `[ ${valores.join(", ")} ]`;
+
+    const rEl = document.querySelector(".vector-r");
+    const r = restaVector(ctba, Object.values(coeficientesFuncion));
+    valores = [];
+    for (let i = 0; i < r.length; i++) {
+        const valor = r[i] % 1 != 0 
+            ? Number(r[i]).toFixed(2) 
+            : r[i];
+        valores.push(valor);
+    }
+    rEl.textContent = `[ ${valores.join(", ")} ]`;
+
+    const tetaEl = document.querySelector(".vector-teta");
+    console.log(bInversa, constantes);
+    const bInversaB = matrizPorVector(bInversa, constantes);
+    const valorEntra = r.indexOf(Math.min(...r));
+    let columna = [];
+    for (let i = 0; i < bInversaA.length; i++) {
+        columna.push(bInversaA[i][valorEntra]);
+    }
+    const teta = dividirVector(bInversaB, columna);
+    valores = [];
+    for (let i = 0; i < teta.length; i++) {
+        const valor = teta[i] % 1 != 0 
+            ? Number(teta[i]).toFixed(2) 
+            : teta[i];
+        valores.push(valor);
+    }
+    tetaEl.textContent = `[ ${valores.join(", ")} ]`;
+
+    const zEl = document.querySelector(".vector-z");
+    let z = vectorPorMatriz(ctb, bInversa);
+    z = multiplicarVector(z, constantes);
+    valores = [];
+    for (let i = 0; i < z.length; i++) {
+        const valor = z[i] % 1 != 0 
+            ? Number(z[i]).toFixed(2) 
+            : z[i];
+        valores.push(valor);
+    }
+    zEl.textContent = `[ ${valores.join(", ")} ]`;
+
+    let minTeta = teta[0];
+    for (let i = 0; i < teta.length ; i++) {
+        if (teta[i] != null && teta[i] < minTeta) {
+            minTeta = teta[i];
+        }
+    }
+    const valorSale = teta.indexOf(minTeta);
+    Xb[valorSale] = variables[valorEntra];
+
+    validarFinIteraciones(r);
+}
+
+function validarFinIteraciones(vector) {
+    const valido = vector.every(v => v >= 0);
+    if (valido) {
+        boton_continuar.style.display = "none";
+        mensaje_fin.style.display = "initial";
+    }
+}
+
+function dividirVector(vector1, vector2) {
+    let resultado = [];
+    for (let i = 0; i < vector1.length; i++) {
+        const valor = vector1[i] / vector2[i];
+        resultado.push(valor >= 0 ? valor : null);
+    }
+    return resultado;
+}
+
+function multiplicarVector(vector1, vector2) {
+    let resultado = [];
+    for (let i = 0; i < vector1.length; i++) {
+        resultado.push(vector1[i] * vector2[i]);
+    }
+    return resultado;
+}
+
+function restaVector(vector1, vector2) {
+    let resultado = [];
+    for (let i = 0; i < vector1.length; i++) {
+        resultado.push(vector1[i] - vector2[i]);
+    }
+    return resultado;
+}
+
+function vectorPorMatriz(vector, matriz) {
+    // Comprobamos que el número de elementos del vector coincida con el número de filas de la matriz
+    if (vector.length !== matriz.length) {
+      throw new Error("El número de elementos del vector debe coincidir con el número de filas de la matriz");
+    }
+  
+    // Inicializamos el vector resultado
+    let resultado = new Array(matriz[0].length).fill(0);
+  
+    // Realizamos la multiplicación
+    for (let j = 0; j < matriz[0].length; j++) {
+      for (let i = 0; i < vector.length; i++) {
+        resultado[j] += vector[i] * matriz[i][j];
+      }
+    }
+  
+    return resultado;
+}
+
+function matrizPorVector(matriz, vector) {
+    // Comprobamos que el número de columnas de la matriz coincida con el tamaño del vector
+    if (matriz[0].length !== vector.length) {
+      throw new Error("El número de columnas de la matriz debe coincidir con el tamaño del vector");
+    }
+  
+    // Inicializamos el vector resultado
+    let resultado = new Array(matriz.length).fill(0);
+  
+    // Realizamos la multiplicación
+    for (let i = 0; i < matriz.length; i++) {
+      for (let j = 0; j < vector.length; j++) {
+        resultado[i] += matriz[i][j] * vector[j];
+      }
+    }
+  
+    return resultado;
+}
+
+function multiplicarMatrices(matrizA, matrizB) {
+    const filasA = matrizA.length;
+    const columnasA = matrizA[0].length;
+    const filasB = matrizB.length;
+    const columnasB = matrizB[0].length;
+  
+    // Verificar que las matrices se pueden multiplicar
+    if (columnasA !== filasB) {
+      throw new Error("El número de columnas de la primera matriz debe ser igual al número de filas de la segunda matriz.");
+    }
+  
+    // Crear la matriz resultado con ceros
+    const resultado = Array(filasA).fill().map(() => Array(columnasB).fill(0));
+  
+    // Realizar la multiplicación de matrices
+    for (let i = 0; i < filasA; i++) {
+      for (let j = 0; j < columnasB; j++) {
+        for (let k = 0; k < columnasA; k++) {
+          resultado[i][j] += matrizA[i][k] * matrizB[k][j];
+        }
+      }
+    }
+  
+    return resultado;
+  }
+
+function getMatrizIdentidad(matriz) {
+    let posiciones = [];
+    let columnas = [];
+    const rows = matriz.length;
+    const cols = matriz[0].length;
+
+    for (let i = 0; i < cols; i++) {
+        let columna = [];
+        for (let j = 0; j < rows; j++) {
+            columna.push(matriz[j][i]);
+        }
+        if (getColumnaIdentidad(columna)) {
+            posiciones.push(i);
+            columnas.push(columna);
+        }
+    }
+    return {posiciones, columnas}
+}
+
+function getColumnaIdentidad(columna) {
+    // Filtra los elementos que son iguales a 1
+    const unos = columna.filter(element => element === 1);
+
+    // Verifica que hay exactamente un 1 y que todos los otros elementos son 0
+    const esValido = unos.length === 1 && columna.every(element => element === 0 || element === 1);
+
+    return esValido;
 }
 
 function setVentanaResultados() {
@@ -249,7 +535,3 @@ function ocultarElemento(elemento) {
 function mostrarElemento(elemento, displayMode = "flex") {
     elemento.style.display = displayMode;
 }
-
-
-
-
